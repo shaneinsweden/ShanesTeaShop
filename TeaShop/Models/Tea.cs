7a75a0a3c2241e6942165eabd4bf12930148ab6e
shane murnion <shaneinsweden@gmail.com>
2019-02-19 15:36:50 +0100

    Added EF

--- TeaShop/Models/Tea.cs
+++ TeaShop/Models/Tea.cs
@@ -12,7 +12,6 @@ namespace TeaShop.Models
         public string Name { get; set; }
         public string ShortDescription { get; set; }
         public string LongDescription { get; set; }
-        public string AllergyInformation { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
         public string ImageThumbnailUrl { get; set; }
