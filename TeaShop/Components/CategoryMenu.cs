1fc500e84dc37f0d5f702ae5c8214d80f1727496
shane murnion <shaneinsweden@gmail.com>
2019-02-24 21:28:40 +0100

    added order component

---
+++ TeaShop/Components/CategoryMenu.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeaShop.Models;
+
+namespace TeaShop.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private ICategoryRepository _categoryRepository;
+
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.Categories.OrderBy(c => c.CategoryName);
+
+            return View(categories);
+        }
+    }
+}
